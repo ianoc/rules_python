55e5625756dcdf0ad37a88c2d30dbdd61f812f0c
Ian O Connell <ioconnell@netflix.com>
2022-06-01 12:59:00 -0600

    Revert "Init: pypa/installer (#700)"
    This reverts commit 9b8ab1ea442064ee5ae5cafc1b66137281813c81.

--- python/pip_install/extract_wheels/lib/bazel.py
+++ python/pip_install/extract_wheels/lib/bazel.py
@@ -21,28 +21,29 @@ WHEEL_ENTRY_POINT_PREFIX = "rules_python_wheel_entry_point"
 
 
 def generate_entry_point_contents(
-    module: str, attribute: str, shebang: str = "#!/usr/bin/env python3"
+    entry_point: str, shebang: str = "#!/usr/bin/env python3"
 ) -> str:
     """Generate the contents of an entry point script.
 
     Args:
-        module (str): The name of the module to use.
-        attribute (str): The name of the attribute to call.
+        entry_point (str): The name of the entry point as show in the
+            `console_scripts` section of `entry_point.txt`.
         shebang (str, optional): The shebang to use for the entry point python
             file.
 
     Returns:
         str: A string of python code.
     """
+    module, method = entry_point.split(":", 1)
     return textwrap.dedent(
         """\
         {shebang}
         import sys
-        from {module} import {attribute}
+        from {module} import {method}
         if __name__ == "__main__":
-            sys.exit({attribute}())
+            sys.exit({method}())
         """.format(
-            shebang=shebang, module=module, attribute=attribute
+            shebang=shebang, module=module, method=method
         )
     )
 
@@ -407,14 +408,14 @@ def extract_wheel(
 
     directory_path = Path(directory)
     entry_points = []
-    for name, (module, attribute) in sorted(whl.entry_points().items()):
+    for name, entry_point in sorted(whl.entry_points().items()):
         # There is an extreme edge-case with entry_points that end with `.py`
         # See: https://github.com/bazelbuild/bazel/blob/09c621e4cf5b968f4c6cdf905ab142d5961f9ddc/src/test/java/com/google/devtools/build/lib/rules/python/PyBinaryConfiguredTargetTest.java#L174
         entry_point_without_py = name[:-3] if name.endswith(".py") else name
         entry_point_target_name = f"{WHEEL_ENTRY_POINT_PREFIX}_{entry_point_without_py}"
         entry_point_script_name = f"{entry_point_target_name}.py"
         (directory_path / entry_point_script_name).write_text(
-            generate_entry_point_contents(module, attribute)
+            generate_entry_point_contents(entry_point)
         )
         entry_points.append(
             generate_entry_point_rule(
@@ -453,7 +454,7 @@ def extract_wheel(
             data_exclude=data_exclude,
             data=data,
             srcs_exclude=srcs_exclude,
-            tags=["pypi_name=" + whl.name, "pypi_version=" + whl.version],
+            tags=["pypi_name=" + whl.name, "pypi_version=" + whl.metadata.version],
             additional_content=additional_content,
         )
         build_file.write(contents)
